bca3678bca9667f507b260eb528e9f4d27b9bfaa
EddieSKim <edkm1212@gmail.com>
2023-12-29 11:10:43 -0700

    Adding svg for banner image

--- portfolio/src/components/banner/Banner.js
+++ portfolio/src/components/banner/Banner.js
@@ -1,13 +1,17 @@
 import React from "react";
-import "./BannerStyle.css";
 import computer from "../../images/banner/computer.png";
 import { TypeAnimation } from "react-type-animation";
+import BannerLogo from "./bannerLogo";
+import styles from "./Banner.module.css";
 
 function Banner() {
     return (
-        <div className="banner-section" id="banner">
-            <img className="banner-image" src={computer} alt="computer-img"/>
-            <h3 className="banner-title">
+        <div className={styles.bannerSection} id="banner">
+            {/* <img className={styles.bannerImage} src={computer} alt="computer-img"/> */}
+            <div className={styles.bannerImage}>
+                <BannerLogo />
+            </div>
+            <h3 className={styles.bannerTitle}>
                 <span>&lt;</span>
                 <TypeAnimation
                     sequence={[
